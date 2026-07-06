122a1b4b79df8bb57a3acb042e9c0500fe2fd896
ikitaii <kiselkata339@gmail.com>
2026-07-06 17:34:14 +0300

    added the second model

---
+++ CatalogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MangaWorld.DTO;
+
+namespace MangaWorld.Models
+{
+    public class CatalogViewModel
+    {
+        public CatalogQueryDto Query { get; set; } = new();
+
+        public IReadOnlyList<Manga> Mangas { get; set; } = Array.Empty<Manga>();
+
+        public IReadOnlyList<CatalogFilterOptionViewModel> Genres { get; set; } = Array.Empty<CatalogFilterOptionViewModel>();
+
+        public IReadOnlyList<CatalogFilterOptionViewModel> Tags { get; set; } = Array.Empty<CatalogFilterOptionViewModel>();
+    }
+}
